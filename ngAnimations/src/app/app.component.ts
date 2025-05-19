385945fff9c0e3f65b479b67abfabaaec1af216a
Thomas Lajoie <2371451@cegepmontpetit.ca>
2025-05-19 09:51:30 -0400

    Animations

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -3,14 +3,53 @@ import { Component } from '@angular/core';
 import { bounce, shakeX, tada } from 'ng-animate';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: true
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('shake', [transition(':increment', useAnimation(shakeX, {
+      params: { timing: 2.0 }
+    }))]),
+    trigger('bounce', [transition(':increment', useAnimation(bounce, {
+      params: { timing: 4.0 }
+    }))]),
+    trigger('tada', [transition(':increment', useAnimation(tada, {
+      params: { timing: 3.0 }
+    }))])
+  ]
 })
 export class AppComponent {
   title = 'ngAnimations';
 
+  rotate= false;
+
+  first = 0;
+  second = 0;
+  third = 0;
+
   constructor() {
+  }
+
+  rotateCube() {
+    if(this.rotate == false){
+      this.rotate = true;
+      setTimeout(() => {this.rotate = false;},2000);
+    }
+  }
+
+  playNgAnimations(forever:boolean) {
+    this.first++;
+    setTimeout(() => {
+      this.second++;
+      setTimeout(() => {
+        this.third++;
+        if(forever)
+        {
+          setTimeout(() => {
+            this.playNgAnimations(true);
+          },3000);
+        }
+      },3000);
+    },2000);
   }
 }
--- ngAnimations/src/main.ts
+++ ngAnimations/src/main.ts
@@ -1,16 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppModule } from './app/app.module';
 
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule),
-        provideAnimations()
-    ]
-})
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
